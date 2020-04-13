392c320aa11f820262b5c5d2dc90eef9344a6639
Ron Martin <ronmartin89@gmail.com>
2020-04-13 14:51:49 -0700

    updated scripts

--- postgrator-config-prod.js
+++ postgrator-config-prod.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': process.env.DATABASE_URL,
+  'connectionString': process.env.DB_URL,
   'ssl': !!process.env.SSL, 
 }
 
