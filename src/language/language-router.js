a24f48f9820e5af90d2a9cc6243060e9266fab86
Ron Martin <ronmartin89@gmail.com>
2020-04-16 09:39:58 -0700

    working on POST /language/guess

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -3,6 +3,7 @@ const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 
 const languageRouter = express.Router()
+const jsonBodyParser = express.json()
 
 languageRouter
   .use(requireAuth)
@@ -66,9 +67,32 @@ languageRouter
   })
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
+  .post('/guess', jsonBodyParser, async (req, res, next) => {
+    const { guess } = req.body
+
+      if (guess == null) {
+        return res.status(400).json({
+            error: `Missing 'guess' in request body`
+        })
+      }
+    
+    try {
+      words = await LanguageService.getLanguageWords(
+          req.app.get('id'),
+          req.language.id
+      )
+
+      const SLL = await LanguageService.createLinkedList(req.language, words)
+
+      
+
+
+      res.send('implement me!')
+      next()
+    } catch (error) {
+        next(error)
+    }
+   
   })
 
 module.exports = languageRouter
