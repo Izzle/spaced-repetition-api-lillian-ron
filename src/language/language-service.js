0af44c458937f70e73fcc362ae10bbf09f0b8182
Ron Martin <ronmartin89@gmail.com>
2020-04-15 17:28:01 -0700

    finished createLinkedList in service

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -47,12 +47,28 @@ const LanguageService = {
 
   getLanguageHead(){},
 
-  createLinkedList(language, word) {
+  createLinkedList(language, words) {
     const SLL = new LinkedList(
       language.id,
       language.user_id,
       language.total_score
     );
+    // language has a 'head' property and words has a 'next'
+    let word = { next: language.head };
+    // every language is saved like a linked list in the DB so there will be a null at the end
+    while(word.next !== null) {
+      word = words.find(word => word.next === word.id);
+      // We use insertLast to keep them in order, since we will want to have them sorted by memory_value
+      SLL.insertLast({
+        id: word.id,
+        original: word.original,
+        translation: word.translation,
+        memory_value: word.memory_value,
+        correct_count: word.correct_count,
+        incorrect_count: word.incorrect_count
+      });
+    }
+    return SLL;
   }
 
 };
